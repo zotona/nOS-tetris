851eaf05fde91f1fe3d3f083579c6810f6694063
vilkov.artem <vilkov.artem@gmail.ru>
2018-07-30 14:51:18 +0500

    first commit

--- tetris/src/api/nos.js
+++ tetris/src/api/nos.js
@@ -1,6 +1,7 @@
 // @flow
 import { TOP_SCORES_SAMPLE } from "./fixtures/TopScores";
-import API from '@nosplatform/api-functions'
+import API from '@nosplatform/api-functions';
+import { Base64 } from 'js-base64';
 
 // Add your smart contract's scriptHash here
 const scriptHash = "fe5e19dc1ac7165ca64f7837b9243d028f943b23";
@@ -12,11 +13,14 @@ const key = "score";
 //export const apiSaveHighScore = (name: string, score: number) =>
 //Promise.resolve();
 
- export const apiGetTopScores = () => API.getStorage({ scriptHash, key });
+ export const apiGetTopScores = () => JSON.parse(Base64.decode(API.getStorage({ scriptHash, key })));
 
- export const apiSaveHighScore = (name: string, score: number) =>
+ export const apiSaveHighScore = (name: string, score: number) => {   
    API.invoke({
      scriptHash,
      operation: "",
      args: [name, score]
    });
+ };
+ 
+
